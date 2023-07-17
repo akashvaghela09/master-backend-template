80663e52b74ef74536d2c7d72ac2c5b2dcea5710
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 02:55:50 +0530

    Update base file

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 6060;
 
-// // Server 1
-const server1 = require('./apps/server1/index.js');
-app.use('/server1', server1);
+// Import your app here
+
 
 app.listen(PORT, () => {
   console.log(`Master backend running at http://localhost:${PORT}`);
