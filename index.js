cb0cea8e596cb8af407ebe56781e13e35211bf30
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 02:15:39 +0530

    Remove test files

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 6060;
 
 // // Server 1
-const server1 = require('./apps/s1/index.js');
+const server1 = require('./apps/server1/index.js');
 app.use('/server1', server1);
-
-// // Server 2
-const server2 = require('./apps/s2/index.js');
-app.use('/server2', server2);
 
 app.listen(PORT, () => {
   console.log(`Master backend running at http://localhost:${PORT}`);
