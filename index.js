2e750463520f50cd35f75741227737808c2c6936
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 03:21:08 +0530

    Add test apps

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 6060;
 
 // Import your app here
+// Server 1
+const server1 = require('./apps/s1/index.js');
+app.use('/server1', server1);
 
+// Server 2
+const server2 = require('./apps/s2/index.js');
+app.use('/server2', server2);
 
 app.listen(PORT, () => {
   console.log(`Master backend running at http://localhost:${PORT}`);
